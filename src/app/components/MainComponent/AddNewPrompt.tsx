c50af3c1e7a1a50a1f5528de3e6b07d124b9e5b0
carlson142 <carlson142@gmail.com>
2025-03-12 15:54:17 +0200

    enhance AddNewPrompt component with hover effect and improved layout

--- src/app/components/MainComponent/AddNewPrompt.tsx
+++ src/app/components/MainComponent/AddNewPrompt.tsx
@@ -2,7 +2,7 @@ import { MdFormatListBulletedAdd } from "react-icons/md";
 
 export default function AddNewPrompt() {
   return (
-    <div className="bg-[#1dd1a1]/20 h-48 border-2 border-dotted border-[#10ac84] rounded-2xl p-2 cursor-pointer">
+    <div className="bg-[#1dd1a1]/20 h-48 border-2 border-dotted border-[#10ac84] rounded-2xl p-2 cursor-pointer flex flex-col hover:bg-[#1dd1a1]/40 transition-all">
       <div className="flex items-center justify-between">
         <div>
           <MdFormatListBulletedAdd />
@@ -11,8 +11,8 @@ export default function AddNewPrompt() {
         <div className="font-bold">Додай свій промпт!</div>
       </div>
 
-      <div className="text-justify">
-        <span>
+      <div className="flex items-center flex-1 text-justify">
+        <span className="">
           Додати новий промпт — це ваш шанс отримати більш точні та релевантні
           відповіді, які відповідають саме вашим потребам. Створіть промпт
           зараз, щоб підвищити ефективність і заощадити час!
