654c2e797d9895280a888909064432317dcfe790
carlson142 <carlson142@gmail.com>
2025-03-12 15:39:50 +0200

    update AddNewPrompt component to include prompt input area

--- src/app/components/MainComponent/AddNewPrompt.tsx
+++ src/app/components/MainComponent/AddNewPrompt.tsx
@@ -1,7 +1,15 @@
+import { MdFormatListBulletedAdd } from "react-icons/md";
+
 export default function AddNewPrompt() {
   return (
-    <div className="bg-[#1dd1a1]/20 h-48 border-2 border-dotted border-[#10ac84] rounded-2xl p-2">
-      AddNewPromp
+    <div className="bg-[#1dd1a1]/20 h-48 border-2 border-dotted border-[#10ac84] rounded-2xl p-2 cursor-pointer">
+      <div className="flex items-center justify-between">
+        <div>
+          <MdFormatListBulletedAdd />
+        </div>
+
+        <div className="font-bold">Додай свій промпт!</div>
+      </div>
     </div>
   );
 }
