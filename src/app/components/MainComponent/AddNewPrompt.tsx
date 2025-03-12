df84723b99d658d2d04c9bde2b5f3842b578a536
carlson142 <carlson142@gmail.com>
2025-03-12 15:31:18 +0200

    add border to new prompt

--- src/app/components/MainComponent/AddNewPrompt.tsx
+++ src/app/components/MainComponent/AddNewPrompt.tsx
@@ -1,3 +1,7 @@
 export default function AddNewPrompt() {
-  return <div className="bg-[#1dd1a1]/20 h-48">AddNewPromp</div>;
+  return (
+    <div className="bg-[#1dd1a1]/20 h-48 border-2 border-dotted border-[#10ac84] rounded-2xl p-2">
+      AddNewPromp
+    </div>
+  );
 }
--- src/app/components/MainComponent/MainComponent.tsx
+++ src/app/components/MainComponent/MainComponent.tsx
@@ -5,14 +5,6 @@ export default function MainComponent() {
     /* FIXME: ВИСОТА!!!! якась хуйня!!! */
     <div className="w-full h-[550px] p-5 grid grid-cols-3 gap-3 overflow-y-auto">
       <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
-      <AddNewPrompt />
     </div>
   );
 }
